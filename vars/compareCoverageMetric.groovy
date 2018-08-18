c4da6279f5a1a30bcf542264918b6085fcc8a80c
Juan Sebastián Urrego Escobar <juancho088@gmail.com>
2018-08-18 04:14:52 +0200

    Comment

--- vars/compareCoverageMetric.groovy
+++ vars/compareCoverageMetric.groovy
@@ -1,16 +1,16 @@
 #!/usr/bin/env groovy
-import com.travelstart.plugins.jenkins.sonar.Coverage
+//import com.travelstart.plugins.jenkins.sonar.Coverage
 
 def call(final Map args) {
 
-    final def coverage = new Coverage(env.SONAR_HOST_URL, env.SONAR_AUTH_TOKEN)
+    //final def coverage = new Coverage(env.SONAR_HOST_URL, env.SONAR_AUTH_TOKEN)
     final def projects = []
     final def newMetric = args.new_coverage ? (args.new_coverage) as Boolean : false
 
     args.originalId ?: projects.add(args.originalId)
     args.newId ?: projects.add(args.originalId)
 
-    def coverageList = coverage.retrieveCodeCoverageMetrics(projects, newMetric)
+    //def coverageList = coverage.retrieveCodeCoverageMetrics(projects, newMetric)
     println("IT WORKS!!!!")
-    println(coverageList)
+    //println(coverageList)
 }
