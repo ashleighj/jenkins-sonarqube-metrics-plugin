4cce60bf22c4d4739136a676dc9ba9fab0987162
Juan Sebastián Urrego Escobar <juancho088@gmail.com>
2018-08-18 04:26:58 +0200

    import that way

--- vars/compareCoverageMetric.groovy
+++ vars/compareCoverageMetric.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
-import com.travelstart.plugins.jenkins.sonar.Coverage
+//import com.travelstart.plugins.jenkins.sonar.Coverage
 
 def call(final Map args) {
 
     //final def coverage = new Coverage(env.SONAR_HOST_URL, env.SONAR_AUTH_TOKEN)
-    //final def coverage = new Coverage("url", "token")
+    final def coverage = new com.travelstart.plugins.jenkins.sonar.Coverage("url", "token")
     final def projects = []
     final def newMetric = args.new_coverage ? (args.new_coverage) as Boolean : false
 
